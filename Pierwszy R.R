ccda82ea6a1a403157be51fc7ce16a2336a58ea7
krzyde <53860063+krzyde@users.noreply.github.com>
2020-11-08 09:18:37 +0100

    gitignore, first script with weathr pi

---
+++ Pierwszy R.R
@@ -0,0 +1,16 @@
+library(devtools)
+library(httr)
+library(jsonlite)
+
+endpoint<-"https://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=ccd2c7f8b414cadf0c4383ce0a541dc2"
+
+getWeather<- GET(endpoint)
+
+weatherText<- content(getWeather,"text")
+
+#View(weatherText)
+
+weatherJson<-fromJSON(weatherText,flatten=TRUE)
+weatherDF <- as.data.frame(weatherJson)
+
+View(weatherDF)

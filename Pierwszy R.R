66cb34ca4ca1e9da4fa375c7c3a5a95978cd21f7
krzyde <53860063+krzyde@users.noreply.github.com>
2020-11-13 15:44:38 +0100

    Added new api <- to github

--- Pierwszy R.R
+++ Pierwszy R.R
@@ -2,17 +2,26 @@ library(devtools)
 library(httr)
 library(jsonlite)
 
-endpoint<-"https://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=ccd2c7f8b414cadf0c4383ce0a541dc2"
+#endpoint<-"https://api.openweathermap.org/data/2.5/weather?q=Lublin&units=metric&appid=ccd2c7f8b414cadf0c4383ce0a541dc2"
 
-getWeather<- GET(endpoint)
+#getWeather<- GET(endpoint)
 
-weatherText<- content(getWeather,"text")
+#weatherText<- content(getWeather,"text")
 
 #View(weatherText)
 
-weatherJson<-fromJSON(weatherText,flatten=TRUE)
-weatherDF <- as.data.frame(weatherJson)
+#weatherJson<-fromJSON(weatherText,flatten=TRUE)
+#weatherDF <- as.data.frame(weatherJson)
 
-View(weatherDF)
+#View(weatherDF)
 
-print(weatherDF)
+#print(weatherDF)
+
+library(httr)
+
+github_api <- function(path) {
+ url <- modify_url("https://api.github.com", path=path)
+ GET(url)
+}
+
+resp <- github_api("/repos/krzyde/NewRProject")
